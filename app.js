fc27faaef8ffd3f1a78d0f426a1ecc7f08fcd1c4
smrdovic <smrdovic@etf.unsa.ba>
2021-06-09 09:19:23 +0200

    Dodatni korak - rucno odobravanje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Pozdrav ARM-saši!'));
+app.get('/', (req, res) => res.send('Dodana ručna revizija.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
